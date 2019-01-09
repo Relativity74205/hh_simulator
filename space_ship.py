e37f774ea6b412c5b540512658379ec88481e96b
Relativity74205 <arkadius@schuchhardt.com>
2019-01-09 22:18:56 +0100

    first code

---
+++ space_ship.py
@@ -0,0 +1,7 @@
+from space_vessel import SpaceVessel
+
+
+class SpaceShip(SpaceVessel):
+
+    def __init__(self):
+        pass
---
+++ space_vessel.py
@@ -0,0 +1,13 @@
+class SpaceVessel:
+    x: float
+    y: float
+    a_x: float
+    a_y: float
+
+    def __init__(self):
+        pass
+
+    def get_acceleration(self):
+        pass
+
+
