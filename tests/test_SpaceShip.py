61efdb28b39c960a70afc1658fdea5da1ac8f5fe
Relativity74205 <arkadius@schuchhardt.com>
2019-08-12 21:53:41 +0200

    started to add tests

--- tests/test_SpaceShip.py
+++ tests/test_SpaceShip.py
@@ -1,13 +1,15 @@
 import pytest
 
-from SpaceShip import SpaceShip, get_compartments_dict
+from SpaceShip import SpaceShip
+from SpaceShip import get_ship_length, get_cnt_broadside_parts, get_weapon_systems, \
+    get_defense_val, get_surplus_weapons_array
 import global_parameters as paras
 from space_objects import ships
 
 
 @pytest.fixture
 def hms_objects():
-    name = 'Cruiser'
+    name = 'test_ship'
     hms = SpaceShip(name)
     hms_dict = ships[name]
 
@@ -20,26 +22,16 @@ def test_space_ship(hms_objects):
     assert isinstance(hms, SpaceShip)
     assert hms.mass == hms_dict['mass']
     assert hms.size == hms_dict['size']
-    assert hms.max_velocity == hms_dict['max_values']['velocity']
-    assert hms.max_acceleration == hms_dict['max_values']['acceleration']
-    assert hms.max_orientation_velocity == hms_dict['max_values']['orientation_velocity']
-    assert hms.max_orientation_acceleration == hms_dict['max_values']['orientation_acceleration']
-    assert hms.max_rotation_velocity == hms_dict['max_values']['rotation_velocity']
-    assert hms.max_rotation_acceleration == hms_dict['max_values']['rotation_acceleration']
+    assert hms.values_max == hms_dict['max_values']
     assert not hms.orientation
     assert not hms.orientation_velocity
     assert not hms.orientation_acceleration
     assert not hms.rotation
     assert not hms.rotation_velocity
     assert not hms.rotation_acceleration
-    assert hms.max_compartment_health == hms_dict['compartment_health']
-    assert len(hms.ship_parts) == int(hms_dict['size']['length']/paras.PART_SIZE) + 2
-    for _, v in hms.ship_parts.items():
-        cs = v['compartments']
-        assert len(cs) == 5
-        assert {'shield_left', 'hull_left', 'hull_right', 'shield_right'}.issubset(cs.keys())
-        for _, c in cs.items():
-            assert c is not None
+    assert hms.compartment_health_max == hms_dict['compartment_health']
+    assert hms.ship_length == 6
+    assert len(hms.compartments) == 0
 
 
 def test_space_ship_bad():
@@ -47,139 +39,28 @@ def test_space_ship_bad():
         SpaceShip('bla')
 
 
+def test_create_compartments():
+    assert False
+
+
+def test_init_parameters():
+    assert False
+
+
 def test_add_weapons():
     assert False
 
 
-def test_create_compartments(hms_objects):
-    pass  # TODO not necessary
+def test_add_weapon_to_compartment():
+    assert False
 
 
-@pytest.mark.parametrize('compartment_name, ship_part_name, health, val', [
-    ('hull_left', 'bow', 1, 1),
-    ('hull_left', 'bow', 2, 2),
-    ('shield_left', 'bow', 1, 0),
-    ('hull_left', 'bow_shield', 1, 0),
-    ('shield_left', 'bow_shield', 1, 0)
-])
-def test_get_compartment_health(compartment_name, ship_part_name, health, val):
-    assert SpaceShip.get_compartment_health(compartment_name, ship_part_name, health) == val
+def test_create_weapon():
+    assert False
 
 
-@pytest.mark.parametrize('direction, shields, comp_name, val', [
-    ('left', {'broadside': 1}, 'shield_left', 1),
-    ('left', {'broadside': 1}, 'shield_right', 0),
-    ('right', {'broadside': 1}, 'shield_left', 0),
-    ('left', {'broadside': 1}, 'hull_left', 0),
-    ('left', {'bow': 1}, 'hull_left', 0),
-    ('bow', {'bow': 2, 'stern': 3}, 'bow_shield', 0)
-])
-def test_get_shield_x(direction, shields, comp_name, val):
-    assert SpaceShip.get_shield_x(direction, shields, comp_name) == val
-
-
-@pytest.mark.parametrize('direction, shields, part_name, val', [
-    ('bow', {'bow': 2, 'stern': 3}, 'bow_shield', 2),
-    ('stern', {'bow': 2, 'stern': 3}, 'bow_shield', 0),
-    ('stern', {'bow': 2, 'stern': 3}, 'stern_shield', 3),
-    ('bow', {'bow': 2, 'stern': 3}, 'bow', 0),
-    ('bow', {'broadside': 3}, 'bow_shield', 0),
-    ('left', {'broadside': 1}, 'shield_left', 0)
-])
-def test_get_shield_y(direction, shields, part_name, val):
-    assert SpaceShip.get_shield_y(direction, shields, part_name) == val
-
-
-@pytest.mark.parametrize('direction, shields, val', [
-    ('top', {'vertical': 1}, 1),
-    ('bottom', {'vertical': 1}, 1),
-    ('bow', {'vertical', 1}, 0),
-    ('top', {'left': 1}, 0)
-])
-def test_get_shield_z(direction, shields, val):
-    assert SpaceShip.get_shield_z(direction, shields) == val
-
-
-@pytest.mark.parametrize('direction, armor, compartment_name, part_name, val', [
-    ('left', {'broadside': 2, 'inner': 1}, 'hull_left', 'bow', 2),
-    ('right', {'broadside': 2, 'inner': 1}, 'hull_left', 'bow', 1),
-    ('right', {'broadside': 2}, 'hull_left', 'bow', 0),
-    ('right', {'broadside': 2, 'inner': 1}, 'hull_right', 'bow', 2),
-    ('left', {'broadside': 2, 'inner': 1}, 'hull_left', 'bow_shield', 0),
-    ('left', {'broadside': 2, 'inner': 1}, 'shield_left', 'bow', 0),
-    ('left', {'broadside': 2, 'inner': 1}, 'middle', 'bow', 1)
-])
-def test_get_armor_x(direction, armor, compartment_name, part_name, val):
-    assert SpaceShip.get_armor_x(direction, armor, compartment_name, part_name) == val
-
-
-@pytest.mark.parametrize('direction, armor, compartment_name, part_name, val', [
-    ('bow', {'bow': 2, 'stern': 3, 'inner': 1}, 'hull_left', 'bow', 2),
-    ('bow', {'bow': 2, 'stern': 3, 'inner': 1}, 'middle', 'bow', 2),
-    ('stern', {'bow': 2, 'stern': 3, 'inner': 1}, 'hull_left', 'bow', 1),
-    ('bow', {'bow': 2, 'stern': 3, 'inner': 1}, 'hull_left', 'stern', 1),
-    ('bow', {'bow': 2, 'stern': 3}, 'hull_left', 'stern', 0),
-    ('stern', {'bow': 2, 'stern': 3, 'inner': 1}, 'hull_left', 'stern', 3),
-    ('bow', {'bow': 2, 'stern': 3, 'inner': 1}, 'middle', 'inner_1', 1),
-    ('stern', {'bow': 2, 'stern': 3, 'inner': 1}, 'middle', 'inner_1', 1),
-    ('stern', {'bow': 2, 'stern': 3, 'inner': 1}, 'middle', 'bow_shield', 0),
-    ('stern', {'bow': 2, 'stern': 3, 'inner': 1}, 'shield_left', 'inner_1', 0)
-])
-def test_get_armor_y(direction, armor, compartment_name, part_name, val):
-    assert SpaceShip.get_armor_y(direction, armor, compartment_name, part_name) == val
-
-
-@pytest.mark.parametrize('direction, armor, compartment_name, part_name, val', [
-    ('top', {'vertical': 2, 'stern': 3, 'inner': 1}, 'hull_left', 'bow', 2),
-    ('left', {'vertical': 2, 'stern': 3, 'inner': 1}, 'hull_left', 'bow', 0),
-    ('top', {'stern': 3, 'inner': 1}, 'hull_left', 'bow', 0),
-    ('bottom', {'vertical': 2, 'stern': 3, 'inner': 1}, 'hull_left', 'bow', 2),
-    ('bottom', {'vertical': 2, 'stern': 3, 'inner': 1}, 'hull_left', 'bow_shield', 0),
-    ('bottom', {'vertical': 2, 'stern': 3, 'inner': 1}, 'shield_left', 'bow', 0)
-])
-def test_get_armor_z(direction, armor, compartment_name, part_name, val):
-    assert SpaceShip.get_armor_z(direction, armor, compartment_name, part_name) == val
-
-
-@pytest.mark.parametrize('length', [
-    300,
-    500,
-    200,
-])
-def test_create_ship_parts(length):
-    ship_part_length = int(length/paras.PART_SIZE)
-    ship_parts = SpaceShip.create_ship_parts(length)
-    ship_parts_keys = ship_parts.keys()
-    assert len(ship_parts_keys) == ship_part_length + 2
-    assert {0, 1, ship_part_length, ship_part_length + 1}.issubset(ship_parts_keys)
-    assert ship_parts[0]['name'] == 'bow_shield'
-    assert ship_parts[1]['name'] == 'bow'
-    assert ship_parts[max(ship_parts_keys)]['name'] == 'stern_shield'
-    assert ship_parts[max(ship_parts_keys) - 1]['name'] == 'stern'
-
-
-@pytest.mark.parametrize('length', [
-    100,
-    0
-])
-def test_create_ship_parts_bad(length):
-    with pytest.raises(ValueError):
-        SpaceShip.create_ship_parts(length)
-
-
-@pytest.mark.parametrize('name', [
-    'asd',
-    '',
-    None
-])
-def test_get_compartments_dict(name):
-    assert get_compartments_dict(name) == {'name': name,
-                                           'compartments': {'shield_left': None,
-                                                            'hull_left': None,
-                                                            'middle': None,
-                                                            'hull_right': None,
-                                                            'shield_right': None
-                                                            }}
+def test_create_compartment_defense(hms_objects):
+    assert False
 
 
 @pytest.mark.parametrize('weapons_cnt, cnt_broadside_parts, cnt_weapons_part, surplus_arr', [
@@ -205,4 +86,59 @@ def test_get_compartments_dict(name):
     [2, 0, 0, []]
 ])
 def test_get_surplus_weapons_array(weapons_cnt, cnt_broadside_parts, cnt_weapons_part, surplus_arr):
-    assert SpaceShip.get_surplus_weapons_array(weapons_cnt, cnt_broadside_parts) == (cnt_weapons_part, surplus_arr)
+    assert get_surplus_weapons_array(weapons_cnt, cnt_broadside_parts) == (cnt_weapons_part, surplus_arr)
+
+
+@pytest.mark.parametrize('size_dict_length, ship_length_units', [
+    (300, 3),
+    (350, 3),
+    (399, 3),
+    (401, 4),
+    (200, 2),
+    (199, ValueError),
+    (1, ValueError),
+    (0, ValueError),
+    (-1, ValueError)
+])
+def test_get_ship_length(size_dict_length, ship_length_units):
+    size_dict = {'length': size_dict_length,
+                 'height': 100,
+                 'width': 100
+                 }
+    if ship_length_units == ValueError:
+        with pytest.raises(ValueError):
+            get_ship_length(size_dict)
+    else:
+        assert get_ship_length(size_dict) == ship_length_units
+
+
+@pytest.mark.parametrize('ship_length, broadside_parts', [
+    (3, 1),
+    (4, 2),
+    (2, 0)
+])
+def test_get_cnt_broadside_parts(ship_length, broadside_parts):
+    assert get_cnt_broadside_parts(ship_length) == broadside_parts
+
+
+def test_get_defense_val():
+    assert False
+
+
+@pytest.mark.parametrize('orientation, weapon_systems', [
+    ('left', [1]),
+    ('right', [1]),
+    ('bow', [2]),
+    ('stern', [3]),
+    ('asd', ValueError)
+])
+def test_get_weapon_systems(orientation, weapon_systems):
+    weapons_dict = {'broadside': [1],
+                    'bow': [2],
+                    'stern': [3]
+                    }
+    if weapon_systems == ValueError:
+        with pytest.raises(ValueError):
+            get_weapon_systems(orientation, weapons_dict)
+    else:
+        assert get_weapon_systems(orientation, weapons_dict) == weapon_systems
