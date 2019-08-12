c49cf14f92da765d1995be998963b1eda4b60aa3
Relativity74205 <arkadius@schuchhardt.com>
2019-08-12 21:53:17 +0200

    added test_ship

--- space_objects.py
+++ space_objects.py
@@ -8,38 +8,38 @@
 # rotation_acceleration: rad/s² ((rad/s)/s)
 
 
-ships = {'Cruiser': {'size': {'length': 600,
-                              'height': 100,
-                              'width': 100
-                              },
-                     'mass': 50000,
-                     'max_values': {'velocity': 100000,
-                                    'acceleration': 500,
-                                    'orientation_velocity': 0.5,
-                                    'orientation_acceleration': 0.1,
-                                    'rotation_velocity': 0.5,
-                                    'rotation_acceleration': 0.1
-                                    },
-                     'weapons': {'broadside': [{'name': 'LaserMkI',
-                                                'amount': 4}],
-                                 'bow': [{'name': 'LaserMkI',
-                                          'amount': 1}],
-                                 'stern': [{'name': 'LaserMkI',
-                                            'amount': 1}]
+ships = {'test_ship': {'size': {'length': 600,
+                                'height': 100,
+                                'width': 100
+                                },
+                       'mass': 50000,
+                       'max_values': {'velocity': 100000,
+                                      'acceleration': 500,
+                                      'orientation_velocity': 0.5,
+                                      'orientation_acceleration': 0.1,
+                                      'rotation_velocity': 0.5,
+                                      'rotation_acceleration': 0.1
+                                      },
+                       'weapons': {'broadside': [{'name': 'LaserMkI',
+                                                  'amount': 4}],
+                                   'bow': [{'name': 'LaserMkI',
+                                            'amount': 1}],
+                                   'stern': [{'name': 'LaserMkI',
+                                              'amount': 1}]
+                                   },
+                       'shields': {'broadside': 100,
+                                   'bow': 0,
+                                   'stern': 0,
+                                   'vertical': -1
+                                   },
+                       'armor': {'broadside': 2,
+                                 'bow': 4,
+                                 'stern': 4,
+                                 'inner': 0.1,
+                                 'vertical': 0
                                  },
-                     'shields': {'broadside': 100,
-                                 'bow': 0,
-                                 'stern': 0,
-                                 'vertical': -1
-                                 },
-                     'armor': {'broadside': 2,
-                               'bow': 4,
-                               'stern': 4,
-                               'inner': 0.1,
-                               'vertical': 0
-                               },
-                     'compartment_health': 10
-                     }
+                       'compartment_health': 10
+                       }
          }
 
 weapon_systems = {'test': {'type': 'test',
